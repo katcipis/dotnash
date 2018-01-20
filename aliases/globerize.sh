5cb0f4a5d7ee21c5df7a1bc169e0bc0dbf2ad4ae
Tiago Katcipis <tiagokatcipis@gmail.com>
2018-01-20 16:30:55 -0200

    Cover non existent files

--- aliases/globerize.sh
+++ aliases/globerize.sh
@@ -3,14 +3,25 @@ fn globerize(args...) {
     globerizedargs = ()
     for arg in $args {
         expandedargs, err <= glob($arg)
-        if $err == "" {
-            for expandedarg in $expandedargs {
-                globerizedargs <= append($globerizedargs, $expandedarg)
-            }
-        } else {
-            globerizedargs <= append($globerizedargs, $arg)
-        }
+        globerizedargs <= _append_glob_res($globerizedargs, $arg, $expandedargs, $err)
     }
-
     return $globerizedargs
+}
+
+fn _append_glob_res(args, arg, expandedargs, err) {
+        if $err != "" {
+            args <= append($args, $arg)
+            return $args
+        }
+
+        if len($expandedargs) == "0" {
+            args <= append($args, $arg)
+            return $args
+        }
+
+        for expandedarg in $expandedargs {
+            args <= append($args, $expandedarg)
+        }
+
+        return $args
 }
