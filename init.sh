86a1d86e6b922bbe91439981946aa06588060312
Tiago Katcipis <tiagokatcipis@gmail.com>
2018-03-26 03:01:15 -0300

    Add basic start of plan9 services

--- init.sh
+++ init.sh
@@ -22,3 +22,25 @@ setenv PATH <= format("%s:%s/bin:%s/bin:%s/bin", $PATH, $GOPATH, $GOROOT, $PLAN9
 setenv SHELL = "/usr/bin/env nash"
 
 refreshPrompt()
+
+fn plan9start() {
+    fn startifnot(name, initfn) {
+	var _, status <= 9p ls $name >[2] /dev/null
+	if $status != "0" {
+	    $initfn()
+	}
+    }
+
+    fn initsources() {
+        9fs sources
+    }
+
+    fn initplumb() {
+        plumber
+    }
+
+    startifnot("sources", $initsources)
+    startifnot("plumb", $initplumb)
+}
+
+plan9start()
