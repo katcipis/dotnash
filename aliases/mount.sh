a43956c4b3c8533e261024becafbf84e4ee334db
Tiago Katcipis <tiagokatcipis@gmail.com>
2018-06-15 17:34:19 -0300

    improve mount aliases

--- aliases/mount.sh
+++ aliases/mount.sh
@@ -1,10 +1,19 @@
 
-fn mountdata() {
-    sudo mount /dev/sda1 /mnt/data
+fn mountdata(number) {
+	if $number == "1" {
+    	sudo mount /dev/sdc1 /mnt/data
+    	return
+    }
+	if $number == "2" {
+		sudo mount /dev/sde1 /mnt/data2
+		return
+	}
+	
+	echo "unknow data disk:" + $number    
 }
 
 fn mountusb() {
-    sudo mount /dev/sde1 /mnt/usb
+    sudo mount /dev/sdf1 /mnt/usb
 }
 
 bindfn mountdata mountdata
