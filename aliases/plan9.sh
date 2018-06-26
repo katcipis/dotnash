5cba5704b7bc3d4eafbe036ca45529fa0eb51496
Tiago Katcipis <tiagokatcipis@gmail.com>
2018-06-26 00:33:04 -0300

    add acme tools

--- aliases/plan9.sh
+++ aliases/plan9.sh
@@ -1,4 +1,6 @@
+
 fn ac() {
+	setenv PATH = $ACMETOOLS + ":" + $PATH
 	acme -c 3 -a -f "/mnt/font/Go Mono/13a/font" -W 2550x1070
 }
 
--- init.sh
+++ init.sh
@@ -12,12 +12,13 @@ import "aliases/mount"
 import "aliases/essentials"
 
 # Setup environment variables
-setenv GOPATH     = $HOME + "/workspace/go"
-setenv GOROOT     = $HOME + "/go"
-setenv NASHPATH   = $HOME + "/nash"
-setenv NASHROOT   = $HOME + "/nashroot"
-setenv PYTHONROOT = $HOME + "/.local/bin"
+setenv GOPATH = $HOME+"/workspace/go"
+setenv GOROOT = $HOME+"/go"
+setenv NASHPATH = $HOME+"/nash"
+setenv NASHROOT = $HOME+"/nashroot"
+setenv PYTHONROOT = $HOME+"/.local/bin"
 setenv PLAN9 = "/usr/local/plan9"
+setenv ACMETOOLS = $PLAN9+"/acme/edit"
 setenv PATH <= format("%s:%s/bin:%s/bin:%s/bin", $PATH, $GOPATH, $GOROOT, $PLAN9)
 setenv SHELL = "/usr/bin/env nash"
 
