61ed3be7f33676a7c3041a0a2b5b34ab626be9ed
Tiago Katcipis <tiagokatcipis@gmail.com>
2018-08-01 14:00:43 -0300

    Improve cfg

--- aliases/acme.sh
+++ aliases/acme.sh
@@ -1,24 +1,14 @@
-
 fn ac() {
-	var cfgfmt = "%s
-/mnt/font/Go Mono/13a/font
-/lib/font/bit/lucm/unicode.9.font
-  0.0000000  12.1690141  39.9436620  72.1126761
-w Newcol Kill Putall Dump Exit 
-c          0 New Cut Paste Snarf Sort Zerox Delcol putf
-c          1 New Cut Paste Snarf Sort Zerox Delcol putf
-c          2 New Cut Paste Snarf Sort Zerox Delcol putf
-c          3 New Cut Paste Snarf Sort Zerox Delcol putf
-f          0           1           0           0   1.9980971 
-          1          48          42           1           0 %s Del Snarf Get | Look putf"
-          
-    var wd <= pwd
-    var cfg <= format($cfgfmt, $wd, $wd)
-    var cfgfile <= format("%s/%s", $HOME, "acme")
-    
-    echo $cfg > $cfgfile
+	var cfgfmt = "%s\n/mnt/font/Go Mono/13a/font\n/lib/font/bit/lucm/unicode.9.font\n  0.0000000  12.6760563  42.2253521  72.0281690\nw Newcol Kill Putall Dump Exit \nc          0 New Cut Paste Snarf Sort Zerox Delcol \nc          1 New Cut Paste Snarf Sort Zerox Delcol \nc          2 New Cut Paste Snarf Sort Zerox Delcol \nc          3 New Cut Paste Snarf Sort Zerox Delcol \nf          0           1           0           0   2.0019066 \n          1          55          56           1           0 %s/ Del Snarf Get | Look"
 
-	setenv PATH = $ACMETOOLS + ":" + $PATH
+	var wd <= pwd
+	var cfg <= format($cfgfmt, $wd, $wd)
+	var cfgfile <= format("%s/%s", $HOME, "acme")
+
+	echo $cfg > $cfgfile
+
+	setenv PATH = $ACMETOOLS+":"+$PATH
+
 	# acme -c 4 -a -f "/mnt/font/Go Mono/13a/font" -W 3550x1070
 	acme -l $cfgfile -W "3550x1070"
 }
