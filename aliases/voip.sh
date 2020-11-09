cd4772a00dbbd3dc334907eb01391d1ced32ba33
Tiago Katcipis <tiagokatcipis@gmail.com>
2020-11-09 21:29:53 +0100

    Fix call burguesia

--- aliases/voip.sh
+++ aliases/voip.sh
@@ -7,7 +7,8 @@ fn call(host) {
 fn callburguesa(host) {
     var host <= format("host=%s", $host)
 
-    gst-launch-1.0 -v pulsesrc ! opusenc ! rtpopuspay ! rtpjitterbuffer ! udpsink $host "port=5001" "async=false" udpsrc "port=5001" "caps=application/x-rtp,media=audio,payload=96,clock-rate=48000,encoding-name=OPUS" ! rtpjitterbuffer ! rtpopusdepay ! pulsesink
+    gst-launch-1.0 -v pulsesrc ! opusenc ! rtpopuspay ! rtpjitterbuffer ! udpsink $host "port=5002" "async=false" udpsrc "port=5002" "caps=application/x-rtp,media=audio,payload=96,clock-rate=48000,encoding-name=OPUS" ! rtpjitterbuffer ! rtpopusdepay ! opusparse ! opusdec ! pulsesink
 }
 
 bindfn call call
+bindfn callburguesa callburguesa
