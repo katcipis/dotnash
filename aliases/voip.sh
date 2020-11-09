c654b05d593b3edf1fffca9f33f64eb5df70b3d3
Tiago Katcipis <tiagokatcipis@gmail.com>
2020-11-09 20:19:47 +0100

    Fix voip alias

--- aliases/voip.sh
+++ aliases/voip.sh
@@ -1,7 +1,7 @@
-fn call(remote_addr) {
-    var pipeline <= format("pulsesrc ! alawenc ! rtppcmapay ! udpsink host=%s port=5001 async=false udpsrc port=5001 caps=application/x-rtp,media=audio,payload=8,clock-rate=8000,encoding-name=PCMA ! rtppcmadepay ! pulsesink", $remote_addr)
-    print($pipeline + "\n")
-    gst-launch-1.0 -v $pipeline
+fn call(host) {
+    var host <= format("host=%s", $host)
+
+    gst-launch-1.0 -v pulsesrc ! alawenc ! rtppcmapay ! udpsink $host "port=5001" "async=false" udpsrc "port=5001" "caps=application/x-rtp,media=audio,payload=8,clock-rate=8000,encoding-name=PCMA" ! rtppcmadepay ! pulsesink
 }
 
 bindfn call call
