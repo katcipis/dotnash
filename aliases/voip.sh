e052a7f2069c9a235c617654775238b2afeda666
Tiago Katcipis <tiagokatcipis@gmail.com>
2020-11-09 18:55:23 +0100

    Add debug on pipeline string

--- aliases/voip.sh
+++ aliases/voip.sh
@@ -1,5 +1,7 @@
 fn call(remote_addr) {
-    gst-launch-1.0 -v "pulsesrc ! alawenc ! rtppcmapay ! udpsink host=10.0.0.1 port=5001 async=false udpsrc port=5001 caps=application/x-rtp,media=audio,payload=8,clock-rate=8000,encoding-name=PCMA ! rtppcmadepay ! pulsesink"
+    var pipeline <= format("pulsesrc ! alawenc ! rtppcmapay ! udpsink host=%s port=5001 async=false udpsrc port=5001 caps=application/x-rtp,media=audio,payload=8,clock-rate=8000,encoding-name=PCMA ! rtppcmadepay ! pulsesink", $remote_addr)
+    print($pipeline + "\n")
+    gst-launch-1.0 -v $pipeline
 }
 
 bindfn call call
