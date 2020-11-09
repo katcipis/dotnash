20ec9fbc99ca9a4f8e8b1a2bd9435170f281f6fc
Tiago Katcipis <tiagokatcipis@gmail.com>
2020-11-09 20:58:20 +0100

    Add some jitter buffer

--- aliases/voip.sh
+++ aliases/voip.sh
@@ -1,7 +1,7 @@
 fn call(host) {
     var host <= format("host=%s", $host)
 
-    gst-launch-1.0 -v pulsesrc ! alawenc ! rtppcmapay ! udpsink $host "port=5001" "async=false" udpsrc "port=5001" "caps=application/x-rtp,media=audio,payload=8,clock-rate=8000,encoding-name=PCMA" ! rtppcmadepay ! pulsesink
+    gst-launch-1.0 -v pulsesrc ! alawenc ! rtppcmapay ! rtpjitterbuffer ! udpsink $host "port=5001" "async=false" udpsrc "port=5001" "caps=application/x-rtp,media=audio,payload=8,clock-rate=8000,encoding-name=PCMA" ! rtpjitterbuffer ! rtppcmadepay ! pulsesink
 }
 
 bindfn call call
